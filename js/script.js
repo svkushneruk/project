01dd47cb7cd183029a864daf4ff150827c2def1a
Serhiy <svkushneruk@ukr.net>
2019-09-02 13:06:47 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+var leftBorderWidth = 1;
+
+let second = 2;
+
+const pi = 3.14;
+
+console.log(second);
+
+second = 3;
+console.log(second);
+
+console.log(4/0);
+console.log('string'*5);
+let s
+console.log(s);
+
+
+let person = {
+   name: "John",
+   age: 25,
+   isMarried: false
+};
+
+console.log(person.name + ' ' + person.age + ' ' + person.isMarried);
+
+
+let arr = ['plum.png', 'orange.jpeg', 'apple.png'];
+
+console.log(arr[2])
